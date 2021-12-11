7e7ce9cad4a57fc09db4655614fd1438a3d3e2c4
Xspree <56574545+Xspree@users.noreply.github.com>
2021-12-11 00:14:01 -0800

    Initial commit

---
+++ Assets/Scripts/UI/MainMenu_UI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_UI : MonoBehaviour
+{
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnClickQuit()
+    {
+        Debug.Log("You have Quit");
+        Application.Quit();
+    }
+}
