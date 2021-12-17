775412edda25318edd78b8c807b92fdc4f04dd48
Xspree <56574545+Xspree@users.noreply.github.com>
2021-12-17 00:04:49 -0800

    Finished up level two and added in music
    Need finally touches such as UI indicators for player controls and passing food

---
+++ Trying to get Home-local/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    
+
+    public Sound[] sounds;
+    // Start is called before the first frame update
+    public static AudioManager instance;
+
+
+    private void Start()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Play("Theme");
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            
+            Play("PlayGround");
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            Play("AlleyWay");
+        }
+       
+    }
+
+    void Awake()
+    {
+        
+        /*if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);*/
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.group;
+        }
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
---
+++ Trying to get Home-local/Assets/Scripts/UI/TurnOnVictoryUi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnVictoryUi : MonoBehaviour
+{
+    [SerializeField]
+    private ExitLevelScript player3D;
+
+    [SerializeField]
+    private ExitLevelScript player2D;
+
+    [SerializeField]
+    private GameObject victoryUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!victoryUI.activeInHierarchy)
+        {
+            if (player3D.inside && player2D.inside)
+            {
+                victoryUI.SetActive(true);
+            }
+        }
+        
+        
+    }
+}
